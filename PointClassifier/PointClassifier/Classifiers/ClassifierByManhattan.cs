66d99e65c0c8e49fd71901589f0f2a851e12dc07
kioshos <rkioshos@gmail.com>
2025-02-22 20:54:20 +0200

    add calculating probability by Manhattan method

--- PointClassifier/PointClassifier/Classifiers/ClassifierByManhattan.cs
+++ PointClassifier/PointClassifier/Classifiers/ClassifierByManhattan.cs
@@ -1,6 +1,64 @@
-﻿namespace PointClassifier.Classifiers;
+﻿using PointClassifier.Model;
+
+namespace PointClassifier.Classifiers;
 
 public class ClassifierByManhattan
 {
+    private List<double> _distanceByManhattan;
+    private ClassifierHandler _classifierHandler;
+    public ClassifierByManhattan(ThreeDimensionalVector vector)
+    {
+        _classifierHandler = new ClassifierHandler(VectorHandler.GetVectors());
+        _distanceByManhattan = _classifierHandler.GetDistanceByEuclidianMetric(vector);
+    }
     
+    private List<double> InvertedValues()
+    {
+        List<double> invertedValues = new List<double>();
+        
+        foreach (var values in _distanceByManhattan)
+        {
+            invertedValues.Add(1/values);
+        }
+        return invertedValues;
+    }
+
+    private double SumOfInvertedValues()
+    {
+        double sum = 0;
+        foreach (var value in InvertedValues())
+        {
+            sum += value;
+        }
+        return sum;
+    }
+
+    private List<double> ProbabilityOfClusterProximity()
+    {
+        List<double> probability = new List<double>();
+        foreach (var value in InvertedValues())
+        {
+            probability.Add(value / SumOfInvertedValues());
+        }
+        return probability;
+    }
+
+    public double SumOfProbability()
+    {
+        double sum = 0;
+        foreach (var value in ProbabilityOfClusterProximity())
+        {
+            sum += value;
+        }
+        return sum;
+    }
+    public void DisplayProbabilityOfCluster()
+    {
+        int clusterCount = 1;
+        foreach (var probability in ProbabilityOfClusterProximity())
+        {
+            Console.WriteLine($"For cluster: {clusterCount}. Probability: {probability}");
+            clusterCount++;
+        }
+    }
 }
