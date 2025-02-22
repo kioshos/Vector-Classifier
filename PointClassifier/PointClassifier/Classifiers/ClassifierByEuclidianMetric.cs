8b334abe64bc261a2050b9c1481a25abdeb4e09d
kioshos <rkioshos@gmail.com>
2025-02-22 20:52:39 +0200

    add calculating probability by Euclidian Metric

--- PointClassifier/PointClassifier/Classifiers/ClassifierByEuclidianMetric.cs
+++ PointClassifier/PointClassifier/Classifiers/ClassifierByEuclidianMetric.cs
@@ -1,6 +1,64 @@
-﻿namespace PointClassifier.Classifiers;
+﻿using PointClassifier.Model;
+
+namespace PointClassifier.Classifiers;
 
 public class ClassifierByEuclidianMetric
 {
+    private List<double> _distanceByEuclidianMetric;
+    private ClassifierHandler _classifierHandler;
+    public ClassifierByEuclidianMetric(ThreeDimensionalVector vector)
+    {
+        _classifierHandler = new ClassifierHandler(VectorHandler.GetVectors());
+        _distanceByEuclidianMetric = _classifierHandler.GetDistanceByEuclidianMetric(vector);
+    }
     
+    private List<double> InvertedValues()
+    {
+        List<double> invertedValues = new List<double>();
+        
+        foreach (var values in _distanceByEuclidianMetric)
+        {
+            invertedValues.Add(1/values);
+        }
+        return invertedValues;
+    }
+
+    private double SumOfInvertedValues()
+    {
+        double sum = 0;
+        foreach (var value in InvertedValues())
+        {
+            sum += value;
+        }
+        return sum;
+    }
+
+    private List<double> ProbabilityOfClusterProximity()
+    {
+        List<double> probability = new List<double>();
+        foreach (var value in InvertedValues())
+        {
+            probability.Add(value / SumOfInvertedValues());
+        }
+        return probability;
+    }
+
+    public double SumOfProbability()
+    {
+        double sum = 0;
+        foreach (var value in ProbabilityOfClusterProximity())
+        {
+            sum += value;
+        }
+        return sum;
+    }
+    public void DisplayProbabilityOfCluster()
+    {
+        int clusterCount = 1;
+        foreach (var probability in ProbabilityOfClusterProximity())
+        {
+            Console.WriteLine($"For cluster: {clusterCount}. Probability: {probability}");
+            clusterCount++;
+        }
+    }
 }
